a9e1b3c7a5a8e0eb51137ac0e2ffaebc344dd2f4
James Hyatt <jameshyatt@my.tridenttech.edu>
2024-01-23 19:12:45 -0500

    First Commit Added support for actuator, securtiy Updated application.properties

---
+++ src/main/java/com/luv2code/springboot/demo/fitpt/FitptApplication.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.demo.fitpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitptApplication.class, args);
+	}
+
